51057f6b66202ed578530b5ad7c316a45c01d7fa
mezzoky <amezzoky@gmail.com>
2016-06-04 00:34:33 +0800

    update repo

--- index.php
+++ index.php
@@ -11,29 +11,26 @@
 function og_graph_preload_fn($post_id) {
     $username = $_SERVER['PHP_AUTH_USER'];
     $password = $_SERVER['PHP_AUTH_PW'];
-    error_log("dagger: save post hook $post_id");
-    error_log("dagger: $username:$password");
-    $a = is_admin();
-    error_log("dagger: $a");
+    // error_log("dagger: save post hook $post_id");
+    // error_log("dagger: $username:$password");
+
+    // only for REST BASIC AUTH method
     $user = wp_authenticate( $username, $password );
     if ( is_wp_error( $user ) ) {
         error_log("dagger: AUTH ERROR");
         return $post_id;
     }
 
-    $save=true;
-    if ($save) {
-        //Force Facebook update anyway - Our meta box could be hidden - Not really! We'll just update if we got our metabox
-        if (get_post_status($post_id)=='publish') {
-            $fb_debug_url='http://graph.facebook.com/?id='.urlencode(get_permalink($post_id)).'&scrape=true&method=post';
-            $response=wp_remote_get($fb_debug_url);
-            error_log("dagger: sent to fb:og: $fb_debug_url");
-            if (is_wp_error($response)) {
-                error_log("dagger: code(ERR send to fb:og)");
-            } else {
-                $code = $response['response']['code'];
-                error_log("dagger: code($code)");
-            }
+    //Force Facebook update anyway - Our meta box could be hidden - Not really! We'll just update if we got our metabox
+    if (get_post_status($post_id)=='publish') {
+        $fb_debug_url='http://graph.facebook.com/?id='.urlencode(get_permalink($post_id)).'&scrape=true&method=post';
+        $response=wp_remote_get($fb_debug_url);
+        error_log("dagger: sent to fb:og: $fb_debug_url");
+        if (is_wp_error($response)) {
+            error_log("dagger: code(ERR send to fb:og)");
+        } else {
+            $code = $response['response']['code'];
+            error_log("dagger: code($code)");
         }
     }
     return $post_id;
